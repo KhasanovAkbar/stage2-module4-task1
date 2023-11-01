633cb6be5349d7925d1ddcc585f5f68c99f41a85
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-11-01 18:21:51 +0100

    first commit

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -13,7 +13,13 @@ public class Employee {
     private String email;
     private String carNumber;
 
-    public Employee() {
+    public Employee(EmployeeBuilder builder) {
+        this.name = builder.name;
+        this.lastName = builder.lastName;
+        this.position = builder.position;
+        this.phone = builder.phone;
+        this.email = builder.email;
+        this.carNumber = builder.carNumber;
     }
 
     public String getName() {
@@ -69,6 +75,50 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+        //
+        private String name;
+        private String lastName;
+        private String position;
+        private String phone;
+        private String email;
+        private String carNumber;
+
+        public EmployeeBuilder() {
+        }
+
+        public EmployeeBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position) {
+            this.position = position;
+            return this;
+
+        }
+
+        public EmployeeBuilder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public EmployeeBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public EmployeeBuilder setCarNumber(String carNumber) {
+            this.carNumber = carNumber;
+            return this;
+        }
+
+        public Employee builder() {
+            return new Employee(this);
+        }
     }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,40 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    //
+    @Override
+    public Connection createConnection() {
+        //
+        Properties properties = loadConnectionProperties("h2database.properties");
+        String dbUrl = properties.getProperty("db_url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        try {
+            return DriverManager.getConnection(dbUrl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Properties loadConnectionProperties(String propertiesFile) {
+        //
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream(propertiesFile)) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to loan connection properties");
+        }
+        return properties;
+    }
 }
 
